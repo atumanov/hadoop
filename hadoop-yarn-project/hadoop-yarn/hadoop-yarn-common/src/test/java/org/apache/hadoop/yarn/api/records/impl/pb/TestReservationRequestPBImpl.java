265403f59f040209cb3678a890cdc9879d9912ed
Alexey Tumanov <atumanov@gmail.com>
2015-06-29 16:09:45 -0700

    minor

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/api/records/impl/pb/TestReservationRequestPBImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/api/records/impl/pb/TestReservationRequestPBImpl.java
@@ -3,7 +3,6 @@ package org.apache.hadoop.yarn.api.records.impl.pb;
 import org.junit.Assert;
 import org.apache.hadoop.yarn.proto.YarnProtos.ReservationRequestProto;
 import org.junit.After;
-//import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
